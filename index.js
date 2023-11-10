7ff0d4636dedbe2a07d683b06c2fe034329bff9e
Bhalani-Mauli <maulibhalani13@gmail.com>
2023-11-10 17:04:37 +0100

    solved 1st and 2nd iteration

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Dhokla",
+      level: "UltraPro Chef",
+      ingredients: [
+        "gram-flour",
+        "water",
+        "ginger-paste",
+        "chilly-paste",
+        "eno",
+      ],
+      cuisine: "Gujarati",
+      dishType: "breakfast",
+      image: "https://images.media-allrecipes.com/images/75131.jpg",
+      duration: "30",
+      creator: "Mauli",
+      created: 10 / 11 / 20323,
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
